2ad8bdc10938a6caca77b2ec7509bb030876cdc1
Ankit Tanna <ankittanna@hotmail.com>
2017-12-06 08:39:57 +0530

    Creating template and options

--- www/js/index.js
+++ www/js/index.js
@@ -14,8 +14,63 @@ function pageLoaded() {
   init();
 }
 
+function resetPage() {
+    errorElement.style.display = 'none';
+    loading.style.display = 'block';
+}
+
 function init() {
 	resetPage();
 
-	
+	getProfilesList(profilesUrl, success, failure);
+
+	function success(profiles) {
+		// Get all profiles: Even ID and Sorted by last name in ascending order
+	    profilesList = profiles.filter(function(profile) {
+	      return !!profile.id && profile.id%2 === 0;
+	    }).sort(function(a, b) {
+	      return a.name.split(" ")[a.name.split(" ").length === 1 ? 1 : a.name.split(" ").length - 1]
+	              .localeCompare(b.name.split(" ").length === 1 ? 1 : b.name.split(" ")[b.name.split(" ").length - 1]);
+	    }).forEach(function(profile) {
+	      addProfile(profile);
+	    });
+
+	    errorElement.style.display = 'none';
+	    loading.style.display = 'none';
+	}
+
+	function failure(error) {
+		errorElement.style.display = 'block';
+    	loading.style.display = 'none';
+	}
+}
+
+function addProfile(profile) {
+  
+  // Prepares the profile from profileTemplate
+  function createProfile() {
+    var html = profileTemplate, regex;
+    for (prop in profile) {
+      regex = new RegExp('{{' + prop + '}}', 'ig');
+      html = html.replace(regex, profile[prop]);
+      if (prop === 'address') {
+        for (subProp in profile[prop]) {
+          regex = new RegExp('{{'+subProp+'}}', 'ig');
+          html = html.replace(regex, profile[prop][subProp]);
+        }
+      }
+
+      if (prop === 'company') {
+        for (subProp in profile[prop]) {
+          if (subProp === 'name') {
+            regex = new RegExp('{{companyName}}', 'ig');
+            html = html.replace(regex, profile[prop][subProp]);
+          }
+        }
+      }
+    }
+    return html;
+  }
+
+  profilesContainer.innerHTML += createProfile();
 }
